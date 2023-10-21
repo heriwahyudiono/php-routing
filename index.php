6ce71bb438e14701fe7157dee302b5b99596b7b2
heriwahyudiono <heriwhydiono@gmail.com>
2023-10-21 17:50:28 +0700

    first commit

--- index.php
+++ index.php
@@ -1,19 +1,18 @@
 <?php
-// Ambil rute dari parameter GET
+
 $route = isset($_GET['route']) ? $_GET['route'] : '';
 
-// Tentukan rute dan tindakan yang sesuai
 switch ($route) {
     case '':
-        include './views/contact.php';
+        include './views/home.php';
         break;
     case 'about':
-        include 'about.php';
+        include './views/about.php';
         break;
     case 'contact':
-        include 'contact.php';
+        include './views/contact.php';
         break;
     default:
-        include '404.php'; // Tampilkan halaman 404 jika tidak ada rute yang cocok
+        include '404.php'; 
         break;
 }
